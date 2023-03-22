39f6d66a6cfc421a92bb77ffbca57fada48ce506
MYWeidner <mawe7753@colorado.edu>
2023-03-22 17:42:36 +0000

    db_create routed added

--- app.py
+++ app.py
@@ -13,3 +13,27 @@ def testing():
     # close db
     conn.close()
     return "Database Connection Successful"
+
+
+@app.route('/db_create')
+def creating():
+    # connect to db
+    conn = psycopg2.connect("postgres://mayra_lab10_db_user:SgYmkCNREibUmkY09jyIj3aMpN5SI0AX@dpg-cgdj88prh17rd85us37g-a/mayra_lab10_db")
+    # cursor to execute SQL
+    cur = conn.cursor()
+    # create basketball table
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    # commit changes from cursor to db
+    conn.commit()
+    # close db
+    conn.close()
+    return "Basketball Table Successfully Created"
+
